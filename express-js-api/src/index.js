134132a99852cfb5bf8ea1afe1a0a6e86a5ebe91
isavara-training <isavara@gmail.com>
2025-12-11 12:18:46 +0530

    added code

---
+++ express-js-api/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 80;
+const color = 'blue';
+const hostname = os.hostname();
+
+app.get('/', (req, res) => {
+  res.send(`<h1 style="color:${color};">Hello from color-api!</h1>
+            <h2>Hostname: ${hostname}</h2>`);
+});
+
+app.get('/api', (req, res) => {
+  const format = req.query.format;
+
+  if (format === 'json') {
+    return res.json({
+      color,
+      hostname,
+    });
+  } else {
+    return res.send(`COLOR : ${color}, HOSTNAME : ${hostname}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Color API listening on port: ${port}`);
+});
+
